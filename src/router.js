9f3ca21a4f8d34ca200cbacb11f938455b338459
Dharmendra <tripathidharmendra789@gmail.com>
2026-03-28 18:10:04 +0530

    Initial commit: ShoppyGlobe setup with Vite

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+import NotFound from './components/NotFound';
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/product/:id", element: <ProductDetail /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/checkout", element: <Checkout /> },
+  { path: "*", element: <NotFound /> }
+]);
+
+export default router;
