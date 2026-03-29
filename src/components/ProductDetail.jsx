0f0959ba82fdd0fc5f8e9ce1c147e3333bd6b017
Dharmendra <tripathidharmendra789@gmail.com>
2026-03-29 13:14:52 +0530

    Remove redundant loading state initialization in ProductDetail component

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -11,7 +11,6 @@ export default function ProductDetail() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
     fetch(`https://dummyjson.com/products/${id}`)
       .then(res => res.json())
       .then(data => {
